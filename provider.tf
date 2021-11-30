db56a7cf34356ea5ae97663e4c56568da348a5c2
abdullatheef1981 <80037253+abdullatheef1981@users.noreply.github.com>
2021-11-30 23:23:00 +0530

    Update provider.tf
    key got delated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAZU6AKIVRY3LOGP7O"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
